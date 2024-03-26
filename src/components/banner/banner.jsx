95c0ca3b84f57164ae335fac17b3d024f79f0171
Pablo Reyes <chugo940@gmail.com>
2024-03-26 00:44:17 -0600

    responsive mejorado

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -3,8 +3,8 @@ import "./banner.css";
 
 export default function Banner() {
   return (
-    <div>
-      <img className="banner" src="./banner3.png"></img>
+    <div class="banner-container">
+      <img class="banner" src="./banner3.png" alt="Banner" />
     </div>
   );
 }
