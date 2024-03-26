54445b7752688418abe8b6637916001ce052e116
Pablo Reyes <chugo940@gmail.com>
2024-03-25 20:11:49 -0600

    final push

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -3,10 +3,10 @@ import "./banner.css";
 
 export default function Banner() {
   return (
-    <div className="relative">
+    <div>
       {/* <img src="/Banner.png" alt="Banner" className="imagen"></img>
     <img src="/promo.png" alt="Promoción" className="imagen promo"></img> */}
-      <img src="./banner2.png"></img>
+      <img src="./banner3.png"></img>
     </div>
   );
 }
